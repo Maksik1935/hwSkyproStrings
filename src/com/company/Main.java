5c24ae64c2cb6a3bcca1bde3ee6201a9529e18d1
m-kac <m-kachalov@mail.ru>
2022-04-13 21:39:27 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(task1());
+        task2(task1());
+        task3();
+    }
+    static String task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        return fullName;
+    }
+    static void task2(String fullName) {;
+        System.out.println(fullName.toUpperCase());
+    }
+    static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'e');
+        System.out.println(fullName);
     }
 }
